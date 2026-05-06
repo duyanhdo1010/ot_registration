507494850c4b35fc6b49667555dd8c60f40f0246
Duy Anh <duyanhdo1010@gmail.com>
2026-05-06 23:52:17 +0700

    feat(chapter-1): odoo structure added

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'OT Registration',
+    'version': '12.0.1.0.0',
+    'category': 'Human Resources',
+    'summary': 'Quan ly dang ky OT',
+    'depends': ['base', 'mail', 'hr', 'project'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
